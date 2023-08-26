ec1a2dfc62cc534fc35141906d33f5e1822f5d37
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-08-26 22:53:27 +0700

    tipe data

--- latihan.js
+++ latihan.js
@@ -16,3 +16,24 @@ console.log("Nama Depan :", name.slice(0,3));
 console.log("Gabungan :", name.slice(0,3), love);
 console.log("huruf terakhir :", name.slice(-1));
 
+//MENGUBAH TIPE DATA
+console.log("---------------MENGUBAH TIPE DATA---------------------")
+let umur = "25";
+let tinggi="170";
+console.log("umur" + " " + parseInt(umur));
+console.log("Tinggi" + " " + parseFloat(tinggi));
+
+//PENGECEKAN TIPE DATA
+console.log("----------------PENGECEKAN TIPE DATA------------------")
+let typeData1 = true;
+let typeData2 = 123;
+let typeData3 = "hallo";
+let typeData4;
+let typeData5 = ["sapi","kuda","kucing"];
+
+console.log(typeof typeData1); //ini type data boolean
+console.log(typeof typeData2); //ini type data number
+console.log(typeof typeData3); //ini type data string
+console.log(typeof typeData4); //ini type data undefined
+console.log(typeof typeData5); //ini type data object
+
