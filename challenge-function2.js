0fa1bbf7edcd8a67bea411c6b55a6abd5450cb46
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-08-30 23:58:12 +0700

    function calculator

--- challenge-function2.js
+++ challenge-function2.js
@@ -1,4 +1,5 @@
-
+// SOAL 1 MENGGUNAKAN FUNGSI EXPRESSION
+console.log("-----------------------SOAL 1 MENGGUNAKAN FUNGSI EXPRESSION------------------------")
 let dataName="Mia Silvia"
 let dataLove = "Love Java Sript"
 const myName = function(nama, age){
@@ -25,3 +26,48 @@ const firstString = function(toFirstString){
 let thisFirstString = firstString(dataName);
 console.log(thisFirstString); //huruf pertama
 /////////////////////////////////////////
+const firstName=function(toFirstName){
+    return toFirstName.slice(0,3);
+}
+let thisFirstName = firstName(dataName);
+console.log(thisFirstName);
+//////////////////////////////////////////
+const lastString = function(toLastString){
+    return toLastString.slice(-1);
+}
+let thisLastString = lastString(dataName);
+console.log(thisLastString); //huruf terakhir
+/////////////////////////////////////////
+const combideName = function(toCombideName){
+    return toCombideName + " " + dataLove;
+}
+thisCombideName = combideName(dataName);
+console.log(thisCombideName);
+/////////////////////////////////////////
+
+/////////////calculator//////////////////////
+console.log("-----------------------SOAL 2 MEMBUAT FUNCTION KALKULATOR------------------------")
+const num1 = 10;
+const num2 = 2;
+const operator = "+";
+function calculator(num1,operator,num2){
+    if(typeof num1 !== "number" || typeof num2 !== "number"){
+        return "No falid, please input number";
+    }else{}
+  
+if (operator == '+'){
+     return num1 + num2;
+}
+else if (operator == '*'){
+    return num1 * num2;
+}
+else if (operator == '-'){
+    return num1 - num2;
+}
+else {
+    return num1 / num2;
+}
+}
+
+let result =calculator(num1,operator,num2);
+console.log(result);
