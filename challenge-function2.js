8c702ff668103ff2787295e156ab942552ebcbd9
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-08-30 21:04:06 +0700

    soal 1

--- challenge-function2.js
+++ challenge-function2.js
@@ -1,1 +1,27 @@
-first
+
+let dataName="Mia Silvia"
+let dataLove = "Love Java Sript"
+const myName = function(nama, age){
+    return `Nama saya adalah ${nama} dan umur saya ${age}`;
+}
+let nameAndAge = myName(dataName,25);
+console.log(nameAndAge); //nama dan umur
+///////////////////////////////////////////
+const changeToLength=function(toLength){
+    return toLength.length;
+}
+let thisLength = changeToLength(dataName);
+console.log(thisLength) //length
+//////////////////////////////////////////
+const upperCase = function(dataNameUpper){
+    return dataNameUpper.toUpperCase();
+}
+let thisUpperCase = upperCase(dataName);
+console.log(thisUpperCase); //uppercase
+//////////////////////////////////////////
+const firstString = function(toFirstString){
+    return toFirstString.slice(0,1);
+}
+let thisFirstString = firstString(dataName);
+console.log(thisFirstString); //huruf pertama
+/////////////////////////////////////////
